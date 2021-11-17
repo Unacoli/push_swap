1eac92b9e14dd068d2f5dc9b5b80b77e73aa9f42
Nao ARGOUSE <nargouse@student.42.fr>
2021-11-17 15:19:30 +0100

    experiment on sorting

--- srcs/sort_under_5.c
+++ srcs/sort_under_5.c
@@ -6,34 +6,94 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 02:05:29 by nargouse          #+#    #+#             */
-/*   Updated: 2021/11/16 17:23:15 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/11/17 14:41:13 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_under_5(t_stack *a, t_stack *b)
+void	sort_easy_4(t_stack *a, t_stack *b)
 {
-	while (a->len > 3)
+	if (b->tab[0] < a->tab[0])
 	{
-		pb(a, b);
-		test(a, b);
-	}
-	sort_3(a);
-	test(a, b);
-	while (b->len > 0)
-	{
-		if (b->tab[1] > b->tab[0])
-		{
-			sb(b);
-			test(a, b);
-		}
 		pa(a, b);
 		test(a, b);
-		if (a->tab[0] < a->tab[a->len - 1])
+	}
+	else if (b->tab[0] > a->tab[a->len - 1])
+	{
+		pa(a, b);
+		ra(a);
+		test(a, b);
+	}
+	else if (b->tab[0] > a->tab[0] && b->tab[0] < a->tab[1])
+	{
+		ra(a);
+		pa(a, b);
+		rra(a);
+		test(a, b);
+	}
+	return ;
+}
+
+void	sort_4(t_stack *a, t_stack *b)
+{
+	if (a->len == 4)
+	{
+		pb(a, b);
+		if (!((a->tab[0] < a->tab[1]) && (a->tab[1] < a->tab[2])))
+			sort_3(a);
+		if (b->tab[0] < a->tab[0] || b->tab[0] > a->tab[a->len - 1]
+			|| (b->tab[0] > a->tab[0] && b->tab[0] < a->tab[1]))
+			sort_easy_4(a, b);
+		else if (b->tab[0] > a->tab[0] && b->tab[0] > a->tab[1])
 		{
+			rra(a);
+			pa(a, b);
+			ra(a);
 			ra(a);
 			test(a, b);
 		}
+		return ;
 	}
+	else
+		sort_under_5(a, b);
+	return ;
+}
+
+void	sort_under_5(t_stack *a, t_stack *b)
+{
+	if (a->tab[0] > a->tab[1])
+		sa(a);
+	pb(a, b);
+	test(a, b);
+	sort_4(a, b);
+	test(a, b);
+	if (b->tab[0] < a->tab[0] || b->tab[0] > a->tab[a->len - 1] || (b->tab[0] > a->tab[0] && b->tab[0] < a->tab[1]))
+		sort_easy_4(a, b);
+	else if (b->tab[0] < a->tab[0] && b->tab[0] > a->tab[1] && b->tab[0] > a->tab[2])
+	{
+		rra(a);
+		test(a, b);
+		pa(a, b);
+		test(a, b);
+		ra(a);
+		test(a, b);
+		ra(a);
+		test(a, b);
+	}
+	else if (b->tab[0] > a->tab[0] && b->tab[0] > a->tab[1]
+		&& b->tab[0] < a->tab[2])
+	{
+		rra(a);
+		test(a, b);
+		rra(a);
+		test(a, b);
+		pa(a, b);
+		test(a, b);
+		rra(a);
+		test(a, b);
+		rra(a);
+		test(a, b);
+	}
+	return ;
 }
