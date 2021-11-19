0d7bdbc96de3f1766dd6a4d46c7035ef872b1da9
Nao ARGOUSE <nargouse@student.42.fr>
2021-11-19 04:30:40 +0100

    try sorting from chunks

--- srcs/sort_under_100.c
+++ srcs/sort_under_100.c
@@ -6,11 +6,32 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 02:56:21 by nargouse          #+#    #+#             */
-/*   Updated: 2021/11/19 01:16:19 by nargouse         ###   ########.fr       */
+/*   Updated: 2021/11/19 04:27:31 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+size_t	find_max_i(t_stack *b)
+{
+	size_t	i;
+	size_t	max_i;
+	int		max;
+
+	i = 0;
+	max = b->tab[0];
+	max_i = 0;
+	while (i < b->len)
+	{
+		if (b->tab[i] > max)
+		{
+			max = b->tab[i];
+			max_i = i;
+		}
+		i++;
+	}
+	return (max_i);
+}
 
 void	sort_under_100(t_stack *a, t_stack *b)
 {
@@ -31,47 +52,96 @@ void	sort_under_100(t_stack *a, t_stack *b)
 	{
 		chunk_min = min + ((chunk - 1) * (max - min) * 20/100);
 		chunk_max = min + (chunk * (max - min) * 20/100);
+		while (scan_a_top(a, chunk_min, chunk_max) != -1 || a->len != 0)
+		{
+			hold_first = scan_a_top(a, chunk_min, chunk_max);
+			hold_second = scan_a_top(a, chunk_min, chunk_max);
+			if ((size_t)hold_first < a->cap - hold_second)
+			{
+				while (a->tab[hold_first--] != a->tab[a->len - 1])
+					rra(a);
+//				if (b->len == 0)
+//				if (a->tab[a->len - 1] > find_max(b))
+//				{
+//					while (b->tab[find_min_i(b)] != b->tab[0])
+//						rb(b);
+//					pb(a, b);
+//				}
+//				else if (a->tab[a->len - 1] < find_min(b))
+//				{
+//					pb(a, b);
+//					while (b->tab[find_min_i(b)] != b->tab[0])
+//						rb(b);
+//				}
+//				else
+//				{
+//					while (!(a->tab[a->len - 1] > b->tab[0] && a->tab[a->len - 1] < b->tab[b->len - 1]))
+//					{	
+//						rb(b);
+//						pb(a, b);
+//					}
+//				}
+			}
+			else
+			{
+				while (a->tab[hold_second++] != a->tab[a->len - 1])
+					ra(a);
+//				printf("ewo from en dehors de ra\n");
+//				if (b->len == 0)
+//				else if (a->tab[a->len - 1] > find_max(b))
+//				{
+//					while (b->tab[find_min_i(b)] != b->tab[0])
+//						rb(b);
+//					pb(a, b);
+//				}
+//				else if (a->tab[a->len - 1] < find_min(b))
+//				{
+//					pb(a, b);
+//					while (b->tab[find_min_i(b)] != b->tab[0])
+//						rb(b);
+//				}
+//				else
+//				{
+//					while (!(a->tab[a->len - 1] > b->tab[0] && a->tab[a->len - 1] < b->tab[b->len - 1]))
+//					{	
+//						rb(b);
+//						pb(a, b);
+//					}
+//				}
+			}
+			if (a->tab[a->len - 1] > b->tab[a->len - 1])
+				pb(a, b);
+			else
+			{
+				pb(a, b);
+				rb(b);
+			}
+//			if (find_max_i(b) > b->len / 2)
+//			{
+//				while (b->tab[find_max_i(b)] != b->tab[b->len - 1])
+//					rb(b);
+//			}
+//			else
+//			{
+//				while (b->tab[find_max_i(b)] != b->tab[b->len - 1])
+//					rrb(b);
+//			}
+		}
 		printf("%d %d %d\n", chunk, chunk_min, chunk_max);	
 		chunk++;
-		if (scan_a_top(a, chunk_min, chunk_max) == -1)
-			continue ;
-		else
-			hold_first = scan_a_top(a, chunk_min, chunk_max);
-		if (scan_a_bottom(a, chunk_min, chunk_max) != -1)
-				hold_second = scan_a_top(a, chunk_min, chunk_max);
-		if (hold_first < 99 - hold_second)
-			while (a->tab[hold_first--] != a->tab[a->len - 1])
-				rra(a);
-		else
-			while (a->tab[hold_second++] != a->tab[a->len - 1])
-				ra(a);
 	}
-	exit(0);
-
-
-
-
-
-
-
-
-
-
-	while (a->len != 0)
+	while (b->len > 0)
 	{
-		if (find_min_i(a) > a->len / 2)
+		if (find_max_i(b) > b->len / 2)
 		{
-			while (a->tab[find_min_i(a)] != a->tab[a->len - 1])
-				ra(a);
+			while (b->tab[find_max_i(b)] != b->tab[b->len - 1])
+				rb(b);
 		}
 		else
 		{
-			while (a->tab[find_min_i(a)] != a->tab[a->len - 1])
-				rra(a);
+			while (b->tab[find_min_i(b)] != b->tab[b->len - 1])
+				rrb(b);
 		}
-		pb(a, b);
-	}
-	while (b->len != 0)
 		pa(a, b);
-	test(a, b);
+	}
 }
