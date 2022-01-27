edb77c436fdfe101521b03acbad6c9b6dcba7d5b
Nao ARGOUSE <nargouse@student.42.fr>
2022-01-27 16:49:17 +0100

    index

--- srcs/index.c
+++ srcs/index.c
@@ -6,7 +6,7 @@
 /*   By: nargouse <nargouse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 02:32:42 by nargouse          #+#    #+#             */
-/*   Updated: 2022/01/27 00:58:15 by nargouse         ###   ########.fr       */
+/*   Updated: 2022/01/27 16:48:03 by nargouse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,40 @@
 
 static t_stack	*get_next_min(t_stack **a)
 {
+	t_stack	*grid;
+	t_stack	*min;
+	int		verif_min;
 
+	min = NULL;
+	verif_min = 0;
+	grid = *a;
+	min->len = 0;
+	if (grid)
+	{
+		while (grid)
+		{
+			if (((int)grid->index == -1) && (!verif_min || (int)(grid->tab[grid->len]) < (int)(min->tab[min->len])))
+			{
+				min = grid;
+				verif_min = 1;
+				min->len++;
+			}
+			grid = grid->next;
+		}
+	}
+	return (min);
 }
 
 void	assign_index(t_stack **a)
 {
+	t_stack	*grid;
 	int		index;
 
 	index = 0;
-	while (index < (int)(*a)->cap)
+	grid = get_next_min(a);
+	while (grid)
 	{
-		(*a)->index = index++;
+		grid->index = index++;
+		grid = get_next_min(a);
 	}
 }
